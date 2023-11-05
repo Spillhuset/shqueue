29abd114690b38396d1c718503f7640ce61b5e0a
Glenn <glenn@not-tech.support>
2023-11-05 10:32:49 +0000

    fix(infoscreens): remove /data endpoint as it's been replaced with ws
    Signed-off-by: Glenn <glenn@not-tech.support>

--- src/apps/infoscreens/urls.py
+++ src/apps/infoscreens/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-  path("queues/<int:queue_id>/data", views.queue_data, name="queue_data"),
   path("queues/<int:queue_id>/<str:type>", views.show_infoscreen, name="show_infoscreen")
 ]
--- src/apps/infoscreens/views.py
+++ src/apps/infoscreens/views.py
@@ -10,23 +10,3 @@ def show_infoscreen(request, queue_id, type):
   preview = request.GET.get("preview", None) is not None
   try: return render(request, f"infoscreens/{type}.html", {"queue": queue, "preview": preview})
   except: return render(request, "errors/404.html", status=404)
-
-def queue_data(request, queue_id):
-  # django is stupid so we gotta send an array query apparently
-  queue = GameQueue.objects.get(id=queue_id)
-  return JsonResponse({
-    "name": queue.name,
-    "active": queue.active,
-    "capacity": queue.capacity,
-    "seconds_per_turn": queue.seconds_per_turn,
-    "queued": [{
-      "name": person.name,
-      "eta_to_play_in_seconds": person.eta_to_play_in_seconds,
-    } for person in queue.queued],
-    "currently_playing": [{
-      "name": person.name,
-      "paused": person.paused_at is not None,
-      "time_left_in_seconds": person.time_left_in_seconds,
-    } for person in list(queue.currently_playing)],
-    "avg_wait_time_seconds": queue.avg_wait_time_seconds,
-  }, safe=False)
